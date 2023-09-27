e6519459ea7e974c954259b330c7abb097502828
FelipexPereira <felipegabriel722@gmail.com>
2023-09-27 04:23:52 -0300

    Banco_Dados

--- banco_dados/cliente.sql
+++ banco_dados/cliente.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 26/09/2023 às 01:04
+-- Tempo de geração: 27/09/2023 às 09:21
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `cliente` (
-  `id` int(11) NOT NULL,
+  `id` int(2) NOT NULL,
   `nome` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `cpf` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
@@ -44,16 +44,9 @@ CREATE TABLE `cliente` (
 --
 
 INSERT INTO `cliente` (`id`, `nome`, `email`, `cpf`, `rg`, `telefone`, `celular`, `data_nascimento`, `status`) VALUES
-(11, 'Felipe Gabriel São Paulo', 'felipe@gmail.com', '432.452.968-00', '77.777.777-7', '00 00000-0000', '11 11111-1111', '1997-08-10', 1),
-(12, 'Roberto Gabriel São Paulo', 'roberto@gmail.com', '432.452.968-00', '77.777.777-7', '00 00000-0000', '11 11111-1111', '1997-08-10', 0),
-(13, 'João Paulo', 'Joao@hotmail.com', '333.333.333-77', '88.777.777-8', '16 98984-8586', '17 98765-5545', '1999-12-10', 1),
-(14, 'Victória Valentina', 'valentina@gmail.com', '432.456.788.00', '88.555.789.9', '19 89884-5555', '12 4548-8888', '2000-12-01', 1),
-(15, 'Felipe Gabriel São Paulo', 'roberto@gmail.com', '432.452.968-00', '77.777.777-7', '19989188481', '11 11111-1111', '1212-12-12', 1),
-(16, 'Felipe Gabriel São Paulo', 'roberto@gmail.com', '432.452.968-00', '77.777.777-7', '19989188481', '11 11111-1111', '1212-12-12', 1),
-(17, 'Felipe Gabriel São Paulo', 'roberto@gmail.com', '432.452.968-00', '77.777.777-7', '19989188481', '11 11111-1111', '1212-12-12', 1),
-(18, 'Felipe Gabriel São Paulo', 'roberto@gmail.com', '432.452.968-00', '77.777.777-7', '19989188481', '11 11111-1111', '1212-12-12', 1),
-(19, 'Felipe Gabriel São Paulo', 'roberto@gmail.com', '432.452.968-00', '77.777.777-7', '19989188481', '11 11111-1111', '1212-12-12', 1),
-(20, 'Felipe Gabriel São Paulo', 'roberto@gmail.com', '432.452.968-00', '77.777.777-7', '19989188481', '11 11111-1111', '1212-12-12', 1);
+(1, 'Felipe Pereira', 'felipe@gmail.com', '000.000.000-00', '00.000.000-0', '00 0000-0000', '00 00000-0000', '1997-08-10', 0),
+(2, 'Victória Valentina', 'valentina@gmail.com', '111.111.111-11', '11.111.111-1', '11 1111-1111', '11 11111-1111', '1997-11-07', 1),
+(3, 'Roberto Vieira', 'roberto@gmail.com', '222.222.222-22', '22.222.222-2', '22 2222-2222', '22 22222-2222', '1998-10-12', 1);
 
 --
 -- Índices para tabelas despejadas
@@ -73,7 +66,7 @@ ALTER TABLE `cliente`
 -- AUTO_INCREMENT de tabela `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
